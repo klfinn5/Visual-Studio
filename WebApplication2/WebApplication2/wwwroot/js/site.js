e7d78f69b1399082f691a6a5ae244ab1f6803311
Kelsey Finn <klfinn5@gmail.com>
2021-09-26 23:33:23 -0400

    fix

--- WebApplication2/WebApplication2/wwwroot/js/site.js
+++ WebApplication2/WebApplication2/wwwroot/js/site.js
@@ -3,6 +3,24 @@
 
 // Write your Javascript code.
 
+function animate(options) {
+
+    var start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / options.duration;
+        if (timeFraction > 1) timeFraction = 1;
+
+        var progress = options.timing(timeFraction)
+
+        options.draw(progress);
+
+        if (timeFraction < 1) {
+            requestAnimationFrame(animate);
+        }
+
+    });
+}
 
 function makeEaseInOut(timing) {
     return function (timeFraction) {
